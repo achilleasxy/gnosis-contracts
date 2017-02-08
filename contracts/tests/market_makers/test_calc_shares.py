735d89f30d09daac6ec4c535f32670c5c49289fc
Stefan George <noreply@sgeorge.de>
2017-02-08 10:42:25 +0100

    calc b inside calc_shares

--- contracts/tests/market_makers/test_calc_shares.py
+++ contracts/tests/market_makers/test_calc_shares.py
@@ -12,7 +12,8 @@ class TestContract(AbstractTestContract):
         self.deploy_contracts = [self.lmsr_name, self.math_library_name]
 
     @staticmethod
-    def calc_shares(tokens, outcome, share_distribution, b):
+    def calc_shares(tokens, outcome, share_distribution, initial_funding):
+        b = initial_funding/float(math.log(len(share_distribution)))
         return b * math.log(
             sum([math.exp(share_count / b + tokens / b) for share_count in share_distribution]) -
             sum([math.exp(share_count / b) for index, share_count in enumerate(share_distribution) if index != outcome])
@@ -24,9 +25,8 @@ class TestContract(AbstractTestContract):
         initial_funding = self.MIN_MARKET_BALANCE
         share_distribution = [initial_funding, initial_funding]
         number_of_shares = 5*10**18
-        b = initial_funding/float(math.log(len(share_distribution)))
         tokens = self.lmsr.calcCostsBuying(
             "".zfill(64).decode('hex'), initial_funding, share_distribution, outcome, number_of_shares
         )
-        approx_number_of_shares = self.calc_shares(tokens, outcome, share_distribution, b)
+        approx_number_of_shares = self.calc_shares(tokens, outcome, share_distribution, initial_funding)
         self.assertAlmostEqual(number_of_shares/approx_number_of_shares, 1, 3)
